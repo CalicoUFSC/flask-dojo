f98e726f5c78beee6f5fd86ba65f49ff5fe828c1
Ranieri Althoff <ranisalt@gmail.com>
2015-10-07 20:54:23 -0300

    Criar endpoint para limpar os pastes

--- app.py
+++ app.py
@@ -2,22 +2,35 @@ import flask
 from flask import Flask
 
 app = Flask(__name__)
-data = []
+data = {}
 
 @app.route('/save', methods=['post',])
 def save():
+    title = flask.request.values["title"]
     content = flask.request.values["content"]
-    data.append(content)
+    data[title] = content
     return content
 
 @app.route('/')
 def index():
     return  flask.render_template('index.html')
 
-@app.route('/fetch')
+@app.route('/fetchall')
 def fetch():
-    return "\n".join(data)
+    return "\n".join(data.values())
 
+@app.route('/fetch/<title>')
+def fetch_single(title):
+    if title in data:
+        return data[title]
+    else:
+        return flask.abort(404)
+
+@app.route('/clear')
+def clear():
+    global data
+    data = {}
+    return "", 200
 
 if __name__ == '__main__':
     # to make it work with pycharm debugging
--- tests.py
+++ tests.py
@@ -8,19 +8,33 @@ class TestCase(unittest.TestCase):
         self.app = app.test_client()
 
     def test_save(self):
-        result = self.app.post('/save', data={'content':'test'})
+        result = self.app.post('/save', data={'title': 'titulo', 'content': 'test'})
         self.assertEqual(result.data, b'test')
         self.assertEqual(result.status_code, 200)
 
     def test_fetchall(self):
-        result = self.app.get('/fetch')
+        result = self.app.get('/fetchall')
         self.assertEqual(result.data, b'')
         self.assertEqual(result.status_code, 200)
-        self.app.post('/save', data={'content': 'test'})
-        result = self.app.get('/fetch')
+        self.app.post('/save', data={'title': 'titulo', 'content': 'test'})
+        result = self.app.get('/fetchall')
         self.assertEqual(result.data, b'test')
         self.assertEqual(result.status_code, 200)
 
+    def test_fetchsingle(self):
+        result = self.app.get('/fetch/asd')
+        self.assertEqual(result.status_code, 404)
+        self.app.post('/save', data={'title': 'asd', 'content': 'test'})
+        result = self.app.get('/fetch/asd')
+        self.assertEqual(result.data, b'test')
+        self.assertEqual(result.status_code, 200)
+
+    def test_clear(self):
+        self.app.post('/save', data={'title': 'asd', 'content': 'test'})
+        result = self.app.get('/clear')
+        result = self.app.get('/fetchall')
+        self.assertEqual(result.data, b'')
+        self.assertEqual(result.status_code, 200)
 
 
     def tearDown(self):
