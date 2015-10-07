f51842efdf3e9728cc2f1907a2b9a8546b08cdd4
BoppreH <boppreh@gmail.com>
2015-10-07 19:35:13 -0300

    Esqueleto de teste para primalidade feito

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
+import flask
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/<n>')
+def primo(n):
+    return "Yes!!"
 
-
-@app.route('/<name>')
-def hello_name(name):
-    return 'Hello, %s!' % (name,)
 
 
 if __name__ == '__main__':
     # to make it work with pycharm debugging
-    app.run(debug=True, use_debugger=False, use_reloader=False)
+    app.run(debug=True, use_debugger=False)
--- tests.py
+++ tests.py
@@ -7,18 +7,14 @@ class TestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_hello_world(self):
-        result = self.app.get('/')
-        self.assertEqual(b'Hello World!', result.data)
-        self.assertEqual(200, result.status_code)
-
-    def test_hello_name(self):
-        result = self.app.get('/test')
-        self.assertEqual(b'Hello, test!', result.data)
-        self.assertEqual(200, result.status_code)
+    def test_prime(self):
+        result = self.app.get('/1')
+        self.assertEqual(result.data, b"Yes!!")
 
     def tearDown(self):
         pass
+
+
 
 
 if __name__ == '__main__':
